6fcd08e21c079e792de3b938dd81b123431b418f
Igor <iwebwork.adm@gmail.com>
2021-08-14 19:48:15 -0300

    Instalação de bibliotecas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Index from './screens/Index'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Index/>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
