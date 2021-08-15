6e0cd7032f71a61dc0cbc86c28f72e28fe913001
Igor <iwebwork.adm@gmail.com>
2021-08-14 21:01:00 -0300

    Pagina index atualizada

--- src/screens/Index.js
+++ src/screens/Index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import Header from '../components/header'
 import About from '../components/about'
+import Profile from '../components/profile'
 
 export default class Index extends Component {
-
     render() {
         return (
             <section name="body"> 
                 <Header/>
-
                 <About/>
+                <Profile/>
             </section>
         )
     }
