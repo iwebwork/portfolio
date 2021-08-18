bc0a54450cf660975c84831fb244878fd866639a
Igor <iwebwork.adm@gmail.com>
2021-08-18 20:10:55 -0300

    Novo modelo de progress bar escolhido

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -1,32 +1,18 @@
 import React from 'react'
 
-const Progressbar = (props) => {
-    
-    const texto = (str) => {
-
-        if(str <= 25)
-            return 'Conhecimento básico'
-        else if(str >= 26 && str <= 50)
-            return 'Conhecimento intermediário'
-        else if(str >= 51 && str <= 99)
-            return 'Sólido conhecimento'
-        else
-            return 'Domínio total'
-        
-    }
-
+export const Progressbartwo = () => {
     return (
-        <div className="progressbar" style={{ marginBottom:'10px', marginTop:'10px'}}>
-            <label className="">{props.title}</label>
-            <div className="progress-bar progress-bar-striped progress-bar-animated" 
-                role="progressbar" aria-valuenow={props.width} aria-valuemin="0" aria-valuemax="100" 
-                style={{"width":props.width+"%"}}>
-                    {
-                        texto(props.width)
-                    }
+        <div className="col-md-3 col-sm-6">
+            <div className="progress yellow">
+                <span className="progress-left">
+                    <span className="progress-bar"></span>
+                </span>
+                <span className="progress-right">
+                    <span className="progress-bar"></span>
+                </span>
+                <div className="inner-circle"></div>
+                <div className="progress-value"><span>26</span>%</div>
             </div>
         </div>
     )
 }
-
-export default Progressbar
